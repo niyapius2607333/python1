875ccdbee214fe06609d3e97e5522058339bfeef
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-10-01 15:20:14 +0530

    Document header added

---
+++ hellopython.py
@@ -0,0 +1,18 @@
+'''
+Author : Niya Pius
+Date : 01-10-2024
+Python program to get student details
+Version 1.0
+'''
+
+student=input("Enter the student name")
+number= int(input("enter the roll number:"))
+number=number+1
+print("Name of the student"),student
+print("Roll number:",number)
+CG= int(input("CGPA of the student:"))
+CG=CG+1
+print("CGPA of the student",CG)
+
+
+
