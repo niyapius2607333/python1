123fddd8d77c00cfee4e7097c4e73f2f2739f484
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-10-08 14:38:07 +0530

    Add files via upload

---
+++ python programs niya pius.py
@@ -0,0 +1,11 @@
+'''
+Author : Niya Pius
+Date : 05-10-2024
+Python program to input name and age of the user
+Version 1.0
+'''
+name=input("ENTER YOUR NAME")
+age=input("ENTER YOUR AGE")
+print("ENTER YOUR NAME:",name)
+print("ENTER YOUR AGE:",age)
+print("Hello,"+name+"!"+"You are"+" "+age+" "+"years old")
